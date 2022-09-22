edf9531977b46b2700587ea995244cc83c61e554
adam nunez <adam.a.nunez@gmail.com>
2022-09-22 18:23:50 -0500

    updating style in config js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
-$(document).ready(function () {
+$(document).ready(() => {
 
     console.log("Loaded report tweaks config")
     var $modal = $('#external-modules-configure-modal');
 
-    $modal.on('show.bs.modal', function () {
+    $modal.on('show.bs.modal', (el) => {
         // Making sure we are overriding this modules's modal only.
-        if ($(this).data('module') !== ReportTweaks.modulePrefix)
+        if ($(el.currentTarget).data('module') !== ReportTweaks.modulePrefix)
             return;
 
         if (typeof ExternalModules.Settings.prototype.resetConfigInstancesOld === 'undefined')
@@ -20,15 +20,13 @@ $(document).ready(function () {
             // Pretty up the form a bit
             $modal.find('thead').remove();
             $modal.find('tr[field=json]').hide();
-            $modal.find('tr[field=info] a').on('click', function () {
-                $modal.find('tr[field=json]').toggle();
-            });
+            $modal.find('tr[field=info] a').on('click', () => $modal.find('tr[field=json]').toggle());
         };
     });
 
-    $modal.on('hide.bs.modal', function () {
+    $modal.on('hide.bs.modal', (el) => {
         // Making sure we are overriding this modules's modal only.
-        if ($(this).data('module') !== ReportTweaks.modulePrefix)
+        if ($(el.currentTarget).data('module') !== ReportTweaks.modulePrefix)
             return;
         if (typeof ExternalModules.Settings.prototype.resetConfigInstancesOld !== 'undefined')
             ExternalModules.Settings.prototype.resetConfigInstances = ExternalModules.Settings.prototype.resetConfigInstancesOld;
